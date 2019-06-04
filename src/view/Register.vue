403abbbcb222feca2b65c8272d0c49c2c3af8673
pudding.min <xumin0626@sina.com>
2019-06-04 22:47:43 +0800

    feat: 新增注册页面。

---
+++ src/view/Register.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input type="text" v-model="form.username">
+    <input type="password" v-model="form.password">
+    <button>重置</button>
+    <button @click="handleSubmit">注册</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Register',
+  data() {
+    return {
+      form: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  created() {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+    handleSubmit() {
+      console.log(this.form)
+    }
+  }
+}
+</script>
+
